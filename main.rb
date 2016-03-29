a06e3ef978b5e067378770e62035e2959d6a52c2
Daria Sova <todariasova@gmail.com>
2016-03-29 16:05:41 -0700

    Add index page

--- main.rb
+++ main.rb
@@ -2,12 +2,5 @@ require 'sinatra'
 require 'sqlite3'
 
 get '/' do 
-  str = "Welcome to the Airport!" 
-
-  db = SQLite3::Database.open "Airport.db"
-  db.execute( "select * from numbers" ) do |row|
-    str+=row[0]
-  end
-  db.close
-  str 
+  haml :index
 end
